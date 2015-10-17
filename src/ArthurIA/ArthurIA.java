bc1ab3ec6a4177579eaf4746ec2ee8b60836b0a0
Arthouf <arthur.suner@free.fr>
2015-10-17 16:16:55 +0200

    First commit, will it run ?

--- src/ArthurIA/ArthurIA.java
+++ src/ArthurIA/ArthurIA.java
@@ -5,6 +5,8 @@ import Game.Tray;
 import Model.Player;
 import Network.Message;
 
+import java.util.Random;
+
 /**
  * Created by raphael on 16/10/15.
  */
@@ -27,40 +29,70 @@ public class ArthurIA extends Player {
             /*
                 Little examples
             */
-            int line1 = 0;
-            int colonne1 = 0;
-            int line2 = 0;
-            int colonne2 = 2;
             boolean hasBeenPlaced;
+            int cptPawn=0;
 
             /*
-                 if you want to place a brige
+                 if you want to place a pawn
             */
-            hasBeenPlaced = tray.placePawn(line1, colonne1, this.color);
+            /*hasBeenPlaced = tray.placePawn(line1, colonne1, this.color);
             hasBeenPlaced = tray.placePawn(line2, colonne2, this.color);
 
             messageReturned = Integer.toString(line1) + Integer.toString(colonne1);
             messageReturned += "+";
-            messageReturned = Integer.toString(line2) + Integer.toString(colonne2);
+            messageReturned = Integer.toString(line2) + Integer.toString(colonne2);*/
 
 
             /*
                 if place bridge
             */
-            hasBeenPlaced = tray.placeBridge(line1, colonne1, line2, colonne2);
+            /*hasBeenPlaced = tray.placeBridge(line1, colonne1, line2, colonne2);
             messageReturned = Integer.toString(line1) + Integer.toString(colonne1);
             messageReturned += "-";
-            messageReturned = Integer.toString(line2) + Integer.toString(colonne2);
+            messageReturned = Integer.toString(line2) + Integer.toString(colonne2);*/
 
             /*
                 you can use also :
             */
 
             // to know if you can place a pawn
-            boolean possible = tray.getBox(line1, colonne2).pawnAllowedHere(this.color);
+           // boolean possible = tray.getBox(line1, colonne2).pawnAllowedHere(this.color);
 
             // to know if you can place a bridge
-            possible = tray.canBridge(line1, colonne1, line2, colonne2);
+            //possible = tray.canBridge(line1, colonne1, line2, colonne2);
+
+            for(int i =0; i<tray.getSize();i++)
+            {
+                for(int j=0;j<tray.getSize();j++)
+                {
+                    boolean possiblePawn = tray.getBox(i, j).pawnAllowedHere(this.color);
+                    boolean possibleBridge = tray.canBridge(i, j, i, j-2);
+                    if(possibleBridge)
+                    {
+                        hasBeenPlaced=tray.placeBridge(i,j,i,j-2);
+                        messageReturned = Integer.toString(i) + Integer.toString(j);
+                        messageReturned += "-";
+                        messageReturned = Integer.toString(i-2) + Integer.toString(j-2);
+                    }
+                    else if(possiblePawn && cptPawn < 2)
+                    {
+
+                        if(cptPawn==0)
+                        {
+                            hasBeenPlaced=tray.placePawn(i,j,this.color);
+                            messageReturned = Integer.toString(i) + Integer.toString(j);
+                            messageReturned += "+";
+                        }
+                        else
+                        {
+                            messageReturned += Integer.toString(i) + Integer.toString(j);
+                        }
+                        cptPawn++;
+                    }
+                }
+            }
+
+
 
         }
         else {
@@ -72,11 +104,16 @@ public class ArthurIA extends Player {
 
     @Override
     public String chooseColor() {
-        return null;
-
-
-
-
-        // do not delete;
+        String colorChoice = "";
+        Random random = new Random();
+        if (random.nextInt()%2 == 0){
+            this.color = Color.White;
+            colorChoice = Message.WHITE;
+        } else {
+            this.color = Color.Black;
+            colorChoice = Message.BLACK;
+        }
+        System.out.println("Color choosen by IA : " + this.color.name());
+        return colorChoice;
     }
 }
